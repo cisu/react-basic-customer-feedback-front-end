66185417c9613f343c0d8de8f6d84a9056e84850
cisu <49948070+cisu@users.noreply.github.com>
2021-03-14 14:07:27 +0200

    feedback-form

--- src/Feedback.js
+++ src/Feedback.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, Fragment} from 'react';
 
 const Feedback = () => {
   // values: is the variable name
@@ -14,7 +14,6 @@ const Feedback = () => {
   });
 
   // destructuring state variables
-
   const {
     name,
     email,
@@ -25,11 +24,74 @@ const Feedback = () => {
     uploadPhotosButtonText,
   } = values;
 
-  return (
-    <div>
-      <p>feedback-app</p>
-    </div>
+  //   event handler
+  const handleChange = () => {
+    console.log('handle change');
+  };
+
+  const handleSubmit = () => {
+    console.log('handle submit');
+  };
+
+  const feedbackForm = () => (
+    <Fragment>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label className='text-muted'>Description</label>
+
+          <textarea
+            onChange={handleChange('message')}
+            type='text'
+            className='form-control'
+            value={message}
+            required
+          ></textarea>
+        </div>
+
+        <div className='form-group'>
+          <label className='text-muted'>Your Name</label>
+
+          <input
+            className='form-control'
+            type='text'
+            onChange={handleChange('name')}
+            value={name}
+            required
+          />
+        </div>
+
+        <div className='form-group'>
+          <label className='text-muted'>Your email</label>
+
+          <input
+            className='form-control'
+            type='text'
+            onChange={handleChange('email')}
+            value={email}
+            required
+          />
+        </div>
+
+        <div className='form-group'>
+          <label className='text-muted'>Your Phone</label>
+
+          <input
+            className='form-control'
+            type='number'
+            onChange={handleChange('phone')}
+            value={phone}
+            required
+          />
+        </div>
+
+        <button className='btn btn-outline btn-primary btn-block'>
+          {buttonText}
+        </button>
+      </form>
+    </Fragment>
   );
+
+  return <div>{feedbackForm()}</div>;
 };
 
 export default Feedback;
